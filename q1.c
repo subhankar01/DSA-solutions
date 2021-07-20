8367a48f1505e1290392031ffe024bf119531533
Subhankar Sen <SUBHANKARSEN2001@GMAIL.COM>
2021-07-20 17:32:55 +0530

    added q1

---
+++ q1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  int item;
+  struct node* left;
+  struct node* right;
+};
+
+// Inorder traversal
+void inorderTraversal(struct node* root) {
+  if (root == NULL) return;
+  inorderTraversal(root->left);
+  printf("%d ", root->item);
+  inorderTraversal(root->right);
+}
+// Create a new Node
+struct node* createNode(value) {
+  struct node* newNode = malloc(sizeof(struct node));
+  newNode->item = value;
+  newNode->left = NULL;
+  newNode->right = NULL;
+
+  return newNode;
+}
+
+// Insert on the left of the node
+struct node* insertLeft(struct node* root, int value) {
+  root->left = createNode(value);
+  return root->left;
+}
+
+// Insert on the right of the node
+struct node* insertRight(struct node* root, int value) {
+  root->right = createNode(value);
+  return root->right;
+}
+
+int main() {
+  struct node* root = createNode(1);
+  insertLeft(root, 2);
+  insertRight(root, 3);
+  insertLeft(root->left, 4);
+
+  printf("Inorder traversal \n");
+  inorderTraversal(root);
+}
